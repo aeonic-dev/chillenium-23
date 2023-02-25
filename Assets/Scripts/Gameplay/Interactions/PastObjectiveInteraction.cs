a23925e51f5d08ac90779fe5200fe7d914c2d512
aeonic <io@aeonic.design>
2023-02-25 13:11:23 -0600

    Add PastObjectiveInteraction

---
+++ Assets/Scripts/Gameplay/Interactions/PastObjectiveInteraction.cs
@@ -0,0 +1,21 @@
+﻿using Core;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Gameplay.Interactions {
+    public class PastObjectiveInteraction : Interaction {
+        [Tooltip("Time to wait before switching back to the hub scene, after completing the level.")]
+        public float delay;
+        public UnityAction onLevelComplete;
+        
+        public override void Interact() {
+            if (GameManager.GameState == GameState.Level) {
+                Invoke(nameof(CompleteLevel), delay);
+            }
+        }
+        
+        private void CompleteLevel() {
+            GameManager.CompleteLevel();
+        }
+    }
+}
