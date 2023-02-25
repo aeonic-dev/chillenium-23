9aea86a90d8d2867b4a1fd40f23d44309974e2dc
aeonic <io@aeonic.design>
2023-02-25 12:14:13 -0600

    Add missing SceneReference extensions, PresentObjectiveInteraction

---
+++ Assets/Scripts/Gameplay/Interactions/PresentObjectiveInteraction.cs
@@ -0,0 +1,26 @@
+﻿using Core;
+using UnityEngine;
+
+namespace Gameplay.Interactions {
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class PresentObjectiveInteraction : Interaction {
+        public Objective objective;
+        public Sprite completedSprite;
+
+        private SpriteRenderer _renderer;
+        private bool _isComplete;
+        
+        public void Complete() {
+            _isComplete = true;
+            _renderer.sprite = completedSprite;
+        }
+        
+        public override void Interact() {
+            GameManager.StartLevel(objective);
+        }
+        
+        public override bool IsInteractable() {
+            return !_isComplete;
+        }
+    }
+}
