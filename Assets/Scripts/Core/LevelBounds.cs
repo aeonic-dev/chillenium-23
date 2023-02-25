ea993eecfeea2473869b4a6d8b988950bd079480
aeonic <io@aeonic.design>
2023-02-24 22:57:54 -0600

    Add LeveLBounds

---
+++ Assets/Scripts/Core/LevelBounds.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Core {
+    /// <summary>
+    /// Simple camera bounding box that exists in every level.
+    /// </summary>
+    public class CameraBounds : MonoBehaviour {
+        [Tooltip("The point furthest to the left and down that the camera will ever see.")]
+        public Vector2 bottomLeft;
+        [Tooltip("The point furthest to the right and up that the camera will ever see.")]
+        public Vector2 topRight;
+
+        private void OnDrawGizmos() {
+            Vector2 topLeft = new(bottomLeft.x, topRight.y);
+            Vector2 bottomRight = new(topRight.x, bottomLeft.y);
+            
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawLine(bottomLeft, topLeft);
+            Gizmos.DrawLine(topLeft, topRight);
+            Gizmos.DrawLine(topRight, bottomRight);
+            Gizmos.DrawLine(bottomRight, bottomLeft);
+        }
+    }
+}
