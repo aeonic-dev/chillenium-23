6c073a1545b424b0c43f51f257e56c73e73dd4d4
aeonic <io@aeonic.design>
2023-02-25 12:13:50 -0600

    Add GameManager, related core functionality

---
+++ Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Gameplay;
+using Gameplay.Interactions;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Util;
+
+namespace Core {
+    public class GameManager {
+        public static GameState GameState = GameState.Menu;
+        public static Objective CurrentObjective => GameState == GameState.Level ? _currentObjective : null;
+        
+        private static Objective _currentObjective;
+
+        public static void CompleteLevel() {
+            PresentObjectiveInteraction presentObjective = (from obj in Object.FindObjectsOfType<PresentObjectiveInteraction>()
+                where obj.objective == _currentObjective select obj).FirstOrDefault();
+            if (presentObjective != null) presentObjective.Complete();
+        }
+        
+        public static void StartLevel(Objective objective) {
+            if (GameState == GameState.Level) return;
+            
+            _currentObjective = objective;
+            GameState = GameState.Level;
+            SceneHook.Get().QueueStartLevel(objective);
+        }
+
+        public static void OnLevelLoaded(Objective objective) {
+            objective.scene.SetActiveScene();
+        }
+    }
+}
---
+++ Assets/Scripts/Core/SceneHook.cs
@@ -0,0 +1,28 @@
+﻿using Gameplay;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Util;
+
+namespace Core {
+    public class SceneHook : MonoBehaviour {
+        public SceneReference hubScene;
+        
+        private Objective _queuedObjective;
+        
+        public void QueueStartLevel(Objective objective) {
+            _queuedObjective = objective;
+            objective.scene.LoadScene(LoadSceneMode.Additive);
+        }
+
+        public static SceneHook Get() {
+            return FindObjectOfType<SceneHook>();
+        }
+        
+        private void Update() {
+            if (_queuedObjective != null) {
+                GameManager.OnLevelLoaded(_queuedObjective);
+                _queuedObjective = null;
+            }
+        }
+    }
+}
