0bf16e84f0c0883448aae6f9a1e609addcdd7269
aeonic <io@aeonic.design>
2023-02-26 06:36:30 -0600

    Add delay and direction settings for objectives, fix GameManager#StartLevel

--- Assets/Scripts/Gameplay/Objective.cs
+++ Assets/Scripts/Gameplay/Objective.cs
@@ -7,6 +7,10 @@ namespace Gameplay {
         public string objectiveName;
         [Tooltip("Time the player is given for completion, in seconds")]
         public int timer;
+        [Tooltip("Time in seconds before the timer begins")]
+        public float delay;
+        [Tooltip("The direction the tendrils will move")]
+        public Tendrils.Direction direction;
         [Tooltip("The platformer scene for this objectggive")]
         public SceneReference scene;
     }
