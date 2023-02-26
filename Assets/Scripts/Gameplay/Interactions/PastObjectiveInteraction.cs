b0a75a9b2dc1c05e85c6bb4d2b7db52663992c8e
aeonic <io@aeonic.design>
2023-02-26 07:08:05 -0600

    Add delay to present objectives, add UnityEvent to all objectives for animation triggering

--- Assets/Scripts/Gameplay/Interactions/PastObjectiveInteraction.cs
+++ Assets/Scripts/Gameplay/Interactions/PastObjectiveInteraction.cs
@@ -1,16 +1,19 @@
 ﻿using Core;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Gameplay.Interactions {
     public class PastObjectiveInteraction : Interaction {
         [Tooltip("Time to wait before switching back to the hub scene, after completing the level.")]
         public float delay;
+        public UnityEvent onInteract;
         
         private bool _completed;
         
         public override void Interact() {
             if (GameManager.GameState == GameState.Level) {
                 _completed = true;
+                onInteract.Invoke();
                 Invoke(nameof(CompleteLevel), delay);
             }
         }
