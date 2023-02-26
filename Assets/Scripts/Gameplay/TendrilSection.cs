1c2c4a81daa7126a0623d9904d4f8f3cae065854
aeonic <io@aeonic.design>
2023-02-26 06:35:52 -0600

    Add tendril section prefab, Tendrils, TendrilSection

---
+++ Assets/Scripts/Gameplay/TendrilSection.cs
@@ -0,0 +1,14 @@
+﻿using Core;
+using UnityEngine;
+
+namespace Gameplay {
+    [RequireComponent(typeof(Collider2D))]
+    public class TendrilSection : MonoBehaviour {
+        private void OnCollisionEnter2D(Collision2D col) {
+            var player = col.gameObject.GetComponent<Player>();
+            if (player != null) {
+                GameManager.StartLevel(GameManager.CurrentObjective);
+            }
+        }
+    }
+}
