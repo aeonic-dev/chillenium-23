aa28480ac219457213d44bb755f5f4794e5705aa
aeonic <io@aeonic.design>
2023-02-26 15:20:05 -0600

    Add SpriteButton, fix ObjectivesUI

---
+++ Assets/Scripts/UI/SpriteButton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace UI {
+    public class SpriteButton : MonoBehaviour {
+        private void Update() {
+            if (Input.GetMouseButtonDown(0)) {
+                RaycastHit hit;
+                
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+ 
+                if (Physics.Raycast(ray.origin,ray.direction, out hit)) {
+                    hit.transform.gameObject.SendMessage("HandleInput");
+                }
+            }
+        }
+    }
+}
