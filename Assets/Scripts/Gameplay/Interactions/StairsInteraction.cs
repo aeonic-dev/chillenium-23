a93fd53ec2b38993ed4b68c228e8e9ede8acd96f
aeonic <io@aeonic.design>
2023-02-26 07:23:01 -0600

    Add StairsInteraction

---
+++ Assets/Scripts/Gameplay/Interactions/StairsInteraction.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Gameplay.Interactions {
+    public class StairsInteraction : Interaction {
+        public Vector2 position;
+        
+        public override void Interact() {
+            Player.Get().transform.position = transform.position + (Vector3) position;
+        }
+
+        private void OnDrawGizmosSelected() {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireCube(position, Vector3.one * .5f);
+        }
+    }
+}
